f264b7a157d281b34114e7c9f8392ba8a0959f32
Brenna Hettler <brenhet@gmail.com>
2022-10-27 21:55:21 -0400

    test

---
+++ coffee-react/src/CoffeeApp.js
@@ -0,0 +1,9 @@
+<><div className="instructions">
+    <h1>Coffee : Water</h1>
+    <p>Do you know how much water you need to make your morning pourover? Enter the amount of coffee you have in grams below:</p>
+</div>
+<div class="calculations">
+    <input type="text" id="coffeegrams" value="30" />
+        <button onclick="coffeeToWaterRatio()">grams of coffee</button>
+        <p>You will need <span id="waterAmountResult">this many</span> grams of water</p>
+ </div></>
